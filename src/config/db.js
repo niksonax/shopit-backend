bdc59875903f4a1453257917bf9b8a974f1fedbd
niksonax <nik2142ua@gmail.com>
2022-01-20 12:38:59 +0200

    Add database config file and .env example

---
+++ src/config/db.js
@@ -0,0 +1,25 @@
+import pg from 'pg';
+import 'dotenv/config';
+
+const { Pool } = pg;
+
+let localPoolConfig = {
+  user: process.env.DATABASE_USER || 'postgres',
+  password: process.env.DATABASE_PASSWORD || 'postgres',
+  host: 'localhost',
+  port: '5432',
+  database: process.env.DATABASE_NAME || 'shopit',
+};
+
+const poolConfig = process.env.DATABASE_URL
+  ? {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+  : localPoolConfig;
+
+const pool = new Pool(poolConfig);
+
+export default pool;
