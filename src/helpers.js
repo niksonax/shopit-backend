77d289ce96c724cdd95bd23d0ed4eeacccc0da28
niksonax <nik2142ua@gmail.com>
2022-01-28 12:24:18 +0200

    Change expire time of access token to 15 mins

--- src/helpers.js
+++ src/helpers.js
@@ -4,7 +4,7 @@ function jwtTokens({ id, name, email }) {
   const user = { id, name, email };
 
   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: '15s', // small value to check if refresh token is working correct
+    expiresIn: '15m',
   });
   const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {
     expiresIn: '1d',
