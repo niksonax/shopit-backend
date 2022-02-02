b285c699b59be17124c74dcf826f6c6c66ff39b0
niksonax <nik2142ua@gmail.com>
2022-02-02 14:13:02 +0200

    Add queries for new purchases table

--- config/db.sql
+++ config/db.sql
@@ -2,6 +2,16 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE DATABASE shopit;
 
+-- UTILITY
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
 -- USERS TABLE
 CREATE TABLE users(
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
@@ -26,3 +36,20 @@ INSERT INTO products (name, description, price, user_id) VALUES ('Peashooter','H
 INSERT INTO products (name, description, price, user_id) VALUES ('Sunflower', 'Sunflower can''t resist bouncing to the beat. Which beat is that? Why, the life-giving jazzy rhythm of the Earth itself, thumping at a frequency only Sunflower can hear.', 50, 'ac1e0283-0ba5-4fca-9964-24eb3cf80450');
 INSERT INTO products (name, description, price, user_id) VALUES ('Melon-pult', 'There''s no false modesty with Melon-pult. "Sun-for-damage, I deliver the biggest punch on the lawn," he says. "I''m not bragging. Run the numbers. You''ll see."', 300, 'ac1e0283-0ba5-4fca-9964-24eb3cf80450');
 INSERT INTO products (name, description, price, user_id) VALUES ('Cherry Bomb', '"I wanna explode," says Cherry #1. "No, let''s detonate instead!" says his brother, Cherry #2. After intense consultation they agree to explodonate.', 150, '6ec25453-944e-4925-89cd-456014045d85');
+
+
+-- PURCHASES TABLE
+CREATE TABLE purchases(
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  product_id uuid,
+  price NUMERIC(10, 2) NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON purchases
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+INSERT INTO purchases (product_id, price) VALUES ('69ca37ce-bf66-4514-a696-14918521a223','50');
