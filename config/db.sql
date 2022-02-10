9760a406830b1ccc6f979963c9f78120f1d3a0c1
niksonax <nik2142ua@gmail.com>
2022-02-10 17:40:16 +0200

    Add some more seeds for products

--- config/db.sql
+++ config/db.sql
@@ -36,6 +36,10 @@ INSERT INTO products (name, description, price, user_id) VALUES ('Peashooter','H
 INSERT INTO products (name, description, price, user_id) VALUES ('Sunflower', 'Sunflower can''t resist bouncing to the beat. Which beat is that? Why, the life-giving jazzy rhythm of the Earth itself, thumping at a frequency only Sunflower can hear.', 50, 'ac1e0283-0ba5-4fca-9964-24eb3cf80450');
 INSERT INTO products (name, description, price, user_id) VALUES ('Melon-pult', 'There''s no false modesty with Melon-pult. "Sun-for-damage, I deliver the biggest punch on the lawn," he says. "I''m not bragging. Run the numbers. You''ll see."', 300, 'ac1e0283-0ba5-4fca-9964-24eb3cf80450');
 INSERT INTO products (name, description, price, user_id) VALUES ('Cherry Bomb', '"I wanna explode," says Cherry #1. "No, let''s detonate instead!" says his brother, Cherry #2. After intense consultation they agree to explodonate.', 150, '6ec25453-944e-4925-89cd-456014045d85');
+INSERT INTO products (name, description, price, user_id) VALUES ('Chomper', 'Chomper almost got a gig doing stunts for The Little Shop of Horrors but it fell through when his agent demanded too much on the front end. Chomper''s not resentful, though. He says it''s just part of the business.', 150, '6ec25453-944e-4925-89cd-456014045d85');
+INSERT INTO products (name, description, price, user_id) VALUES ('Scaredy-shroom', '"Who''s there?" whispers Scaredy-shroom, voice barely audible. "Go away. I don''t want to see anybody. Unless it''s the man from the circus."', 25, '6ec25453-944e-4925-89cd-456014045d85');
+INSERT INTO products (name, description, price, user_id) VALUES ('Squash', '"I''m ready!" yells Squash. "Let''s do it! Put me in! There''s nobody better! I''m your guy! C''mon! Whaddya waiting for? I need this!"', 50, 'a6288c0d-074b-491f-8d95-ba6fa72f94b6');
+INSERT INTO products (name, description, price, user_id) VALUES ('Jalapeno', '"NNNNNGGGGG!!!!!!!!" Jalapeno says. He''s not going to explode, not this time. But soon. Oh, so soon. It''s close. He knows it, he can feel it, his whole life''s been leading up to this moment.', 125, 'a6288c0d-074b-491f-8d95-ba6fa72f94b6');
 
 
 -- PURCHASES TABLE
