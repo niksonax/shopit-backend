03bbaaf3b7eb60c6dd1d404c0ae39dbcec00ffce
niksonax <nik2142ua@gmail.com>
2022-02-15 17:17:55 +0200

    Create function for granting access depending on the role

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,7 @@
+import RolesModel from './roles/model.js';
+
+const rolesModel = new RolesModel();
+
 function validateRequest(req, res, next, schema) {
   const options = {
     abortEarly: false, // include all errors
@@ -19,4 +23,34 @@ function validateRequest(req, res, next, schema) {
   next();
 }
 
-export { validateRequest };
+function grantAccess(role) {
+  return async (req, res, next) => {
+    const rolesValue = {
+      user: 1,
+      manager: 2,
+      admin: 3,
+    };
+
+    try {
+      const { roleId } = res.locals.user;
+      const { title: userRole } = await rolesModel.getById(roleId);
+
+      if (!rolesValue[role]) throw Error(`${role} doesn't exists`);
+      if (!rolesValue[userRole]) throw Error(`${userRole} doesn't exists`);
+
+      if (rolesValue[userRole] < rolesValue[role]) {
+        throw Error(
+          `You need at least ${role} role to access that route. Role given: ${userRole}`
+        );
+      }
+
+      next();
+    } catch (error) {
+      res.status(401).json({
+        error: error.message,
+      });
+    }
+  };
+}
+
+export { validateRequest, grantAccess };
